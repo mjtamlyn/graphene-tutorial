e594c0d391e2525b3c4ffb5a8b6b7be431de4f94
Marc Tamlyn <marc.tamlyn@gmail.com>
2016-09-17 12:10:41 +0200

    Setup dj-static.

--- village/wsgi.py
+++ village/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "village.settings")
 
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
